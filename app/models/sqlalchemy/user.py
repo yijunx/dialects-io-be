8fb40e4aec0862713e88d7ba18a9f7c58b1f533b
yijunx <yijunx@umich.edu>
2024-10-19 13:02:49 +0000

    update user table

--- app/models/sqlalchemy/user.py
+++ app/models/sqlalchemy/user.py
@@ -1,21 +1,26 @@
 from datetime import datetime
 
-from sqlalchemy import DateTime, ForeignKey, Index, Integer, String
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-
+from sqlalchemy import DateTime, UUID, String
+from sqlalchemy.orm import Mapped, mapped_column
+import uuid
 from app.models.sqlalchemy.base import Base
 
 # these tables are inspired by prisma schema
 
 
-class UserORM(Base):
-    __tablename__ = "users"
-    id: Mapped[str] = mapped_column(String, primary_key=True)
+class UserRecord(Base):
+    __tablename__ = "user"
+
+    # cos keycloak uses uuid...
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True)
+
     name: Mapped[str] = mapped_column(String, nullable=False)
     email: Mapped[str] = mapped_column(String, unique=True, index=True)
+    nick_name: Mapped[str] = mapped_column(String, nullable=True)
+
     role: Mapped[str] = mapped_column(String, nullable=False)
     realm: Mapped[str] = mapped_column(String, nullable=True)
 
-    created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
-    updated_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
-    last_login_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+    create_time: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    update_time: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+    last_login_time: Mapped[datetime] = mapped_column(DateTime, nullable=True)
